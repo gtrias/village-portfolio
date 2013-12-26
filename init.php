8390c459066f304b59276545573bb698553779c4
Norris <im@justnorris.com>
2013-12-26 17:42:03 +0200

    Restore plugin_basename()

--- init.php
+++ init.php
@@ -27,7 +27,7 @@ if (is_admin()) { // note the use of is_admin() to double check that this is hap
 	    require_once( "updater.php" );
 
 	    $config = array(
-	        'slug' => 'village-portfolio/init.php', 
+	        'slug' => plugin_basename( __FILE__ ), 
 	        'proper_folder_name' => 'village-portfolio', // this is the name of the folder your plugin lives in
 	        'api_url' => 'https://api.github.com/repos/justnorris/village-portfolio', // the github API url of your github repo
 	        'raw_url' => 'https://raw.github.com/justnorris/village-portfolio/master', // the github raw url of your github repo
